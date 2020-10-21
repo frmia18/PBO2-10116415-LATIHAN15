e0db267bb28a943bcc838e028fd7cf32293b320b
frmia18 <50294841+frmia18@users.noreply.github.com>
2020-10-21 10:05:51 +0700

    Add files via upload

---
+++ pbo210116415Latihan15Logika/src/pbo210116415latihan15logika/Pbo210116415Latihan15Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210116415latihan15logika;
+
+/**
+ *
+ * @author Frda
+ */
+public class Pbo210116415Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
